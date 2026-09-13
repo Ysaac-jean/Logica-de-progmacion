bfaf05180d1df36c0591dc3304a571ef20d76f9f
Ysaac-jean <cjysaacm@gmail.com>
2026-09-13 06:57:43 +0200

    Add files via upload

---
+++ Diferente o Igual.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+	int main()
+{
+	int a, A;
+	cout << "ingrese dos numeros:"<<endl;
+	cin >> a >> A;
+	if (a == A) {
+		cout << "son iguales" << endl;
+	}
+	else {
+		cout << "son diferentes" << endl;
+	}
+	return 0;
+}
