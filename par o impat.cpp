c084bcb781e2fa4aee03822f6d3ecf5cbb269748
Ysaac-jean <cjysaacm@gmail.com>
2026-09-13 07:04:56 +0200

    Add files via upload

---
+++ par o impat.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "ponga numero" << endl;
+	cin >> n;
+	if (n % 2 == 0)
+	{
+		cout << "es par" << endl;
+	}
+	else {
+		cout << "en impar" << endl;
+	}
+	return 0;
+}
